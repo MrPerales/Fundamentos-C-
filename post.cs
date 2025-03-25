2a55145bdabfcfe6c8dc28bd30346a5d0391440a
Carlos Perales <carlosperales050@gmail.com>
2025-03-24 19:25:02 -0600

    solictud GET

---
+++ post.cs
@@ -0,0 +1,12 @@
+namespace ClassPost
+{
+
+    public class Post
+    {
+        public int userID { set; get; }
+
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+}
