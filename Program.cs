c7570bc91a55d4fc67076d66b26d8d65555f6bb4
Carlos Perales <carlosperales050@gmail.com>
2025-03-26 19:26:47 -0600

    add main

--- Program.cs
+++ Program.cs
@@ -1,80 +1,54 @@
-﻿// formas de crear una istancia de la clase 
-// 1 forma 
-Sale sale = new SaleWithTax(15, 1.16m); //m para indicar que es decimal
-var message = sale.GetInfo();
-// var protected= sale.GetProtected(); //no se puede acceder desde fuera 
-Console.WriteLine(message);
-// sale.Total= 15 //tenemos que tener el set para poder asignar el valor 
+﻿using Fundamentos.conectionDB;
+using Fundamentos.lamdba;
 
-// 2 forma 
-// no confundir "var" con el "var" de JavaScript 
-// var sale= new Sale();
-
-// 3forma 
-// no es tan utilizada 
-// Sale sale = new();
-
-/////////////////////////////////////////////////////////
-///////////////// Herencia de clases /////////////////////
-
-class SaleWithTax : Sale
+namespace Fundamentos
 {
-    public decimal Tax { get; set; }
-    // constructor , ya que la clase Sale necesita un argumento 
-    // se lo mandamos desde el contructor de la clase SaleWithTax
-    // :base(total) con esto le mandamos el argumento a la clase padre 
-    public SaleWithTax(decimal total, decimal tax) : base(total)
+    class Program
     {
-        GetProtected();
-        this.Tax = tax;
-    }
-    public string GetInfoWithTax()
-    {
-        return "El total es" + Total + " Impuesto : " + Tax;
-    }
-    // sobreescritura 
-    // se le agrega "override" para que se puede sobreesctibir 
-    // lo que significa que va a llamar a este metodo y no al metodo del padre
-    public override string GetInfo()
-    {
-        return "El total es " + Total + " Impuesto : " + Tax;
-    }
+        static void Main()
+        {
+            // lambda 
+            // lambdaExamples suma = new lambdaExamples();
+            // suma.Sumar(2, 1);
 
-    // sobrecarga = se puede llamar igual pero con diferentes argumentos 
-    public string GetInfo(string message)
-    {
-        return message;
-    }
-    public string GetInfo(int a)
-    {
-        return " " + a;
-    }
-}
+            // suma.SomeReturn((a, b) => a + b, 5);
 
-class Sale
-{
-    public decimal Total { get; set; } // get and set para obtener y asignar valor
-    // propiedad privada solo se accede de manera local (dentro de la clase)
-    private decimal _some;
+            // Conection a Base de datos 
+            // CervezaBD cervezaBD = new CervezaBD();
 
-    // constructor tiene el mismo nombre de la clase 
-    public Sale(decimal total)
-    {
-        this.Total = total;
+            // agregamos una Cerveza 
+            // {} hack para crear un scope distinto 
+            // {
+            //     var cerveza = new Cerveza(2, "Pale ale");
+            //     cerveza.Marca = "minerva";
+            //     cerveza.Alcohol = 6;
+            //     cervezaBD.Add(cerveza);
+            // }
+            /////////////////////
+            ///editamos una cerveza 
+            // {
+            //     var cerveza = new Cerveza(5, "Pale ale");
+            //     cerveza.Marca = "minerva";
+            //     cerveza.Alcohol = 5;
+            //     cervezaBD.Edit(cerveza, 40);
+            // }
 
+            ////////////////////////////
+            ///eliminamos una cervez 
+            // {
+            //     cervezaBD.Delete(40);
+            // }
 
-    }
-    // para que se pueda hacer sobreescritura se agreda "virtual"
-    public virtual string GetInfo()
-    {
-        return "El total es " + Total;
-    }
+            // Obtenemos todas las cervezas 
+            // var cervezas = cervezaBD.Get();
+            // foreach (var item in cervezas)
+            // {
+            //     Console.WriteLine(item.Name);
 
-    // protected solo se puede acceder desde la clase padre o hijo 
-    protected string GetProtected()
-    {
-        return "soy un protected";
-    }
+            // }
+            Console.WriteLine("-------------------------------");
 
 
+        }
+    }
 }
